9bd947b4121a4de814d85606195200f06a65709b
Matthew Giuggio <matthewgiuggio@Matthews-MacBook-Pro.local>
2017-09-17 01:02:01 -0400

    update 1

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var express = require('express');
+var express = require("express");
 
 var app = express();
+
+console.log('date ' + new Date());
 
 /*
 	REST end point
 	http://localhost:8080/
 */
 app.get("/", function(req, res) {
-	res.sendfile(__dirname + "/index.html")
+	res.sendFile(__dirname + "/index.html");
 });
 
+app.post('/login', function(req, res) {
+	res.end("login successful");
+});
 var port = process.env.PORT || 8080;
-
-//app.get(routename, clbk);
+  
 app.listen(port, function() {
 	console.log("App is running on port " + port);
 });
